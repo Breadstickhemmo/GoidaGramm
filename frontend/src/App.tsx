4925329e24cafba1c0225f9d0bf68fe77a2387e5
Breadstickhemmo <anohin.dima2018@yandex.ru>
2026-03-20 08:03:47 +0700

    Initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Goidagram Messenger</h1>
+        <p>Инициализация системы...</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
